eee2580ef5b0e15a2963d80e73db4b071b81eb7d
SURAJ GIRI <66070164+surajgiri974@users.noreply.github.com>
2025-01-15 02:00:33 +0530

    Update settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6-k(7($k@4y^eotphyhsl$uwsj^8ny^f718e!e$nlebf($*e4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://suraj-giri-portfolio.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
